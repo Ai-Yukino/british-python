57d1df732d5ac13c00d9bbe796375da8a13b74dd
Ai-Yukino <87879276+Ai-Yukino@users.noreply.github.com>
2022-05-07 21:00:44 -0500

    ✅ Finish problem set 04

--- advanced/02-adv/notebooks/04/04.py
+++ advanced/02-adv/notebooks/04/04.py
@@ -20,57 +20,122 @@
 #     <img src="images/04.png" style="width: 60%; border-radius: 2%">
 # </center>
 #
+# [🖼 Thomas Bonometti | Unsplash](https://unsplash.com/photos/OyO5NDiRPMM)
+#
 # ---
 #
 # [👩‍💻 Students_Alcohol_Consumption $\subset$ 04_Apply $\subset$ pandas_exercises](https://github.com/zachhall/pandas_exercises/blob/master/04_Apply/Students_Alcohol_Consumption/Exercises.ipynb)
+
+# + [markdown] tags=[]
+# ## 📝 Table of contents 📝
 #
+# - [❄ Grading tips ❄](#❄)
+# - [🐍 Python imports 🐍](#🐍)
+# - [📁 Data 📁](#📁)
+#     - [🍃 Extract](#🍃)
+#     - [💧🔥 Transform](#💧🔥)
+#     - [🍵 Load](#🍵)
+# - [👀 Problems and solutions 👀](#👀)
+#     - [❄ Step 1. $\dots$](#❄1)
+#         - [🌸 Solution](#🌸1)
+#     - $\vdots$
+#     - [❄ Step 10. $\dots$](#❄10)
+#         - [🌸 Solution](#🌸10)
+
+# + [markdown] tags=[]
+# ## 🐍 Python imports 🐍 <a id="🐍"></a>
 # -
 
-# ## 🐍 Python imports 🐍 <a id="🐍"></a>
-
 import pandas as pd
+import numpy as np
 
-# ## 📁 Data processing 📁 <a id="📁"></a>
+# ## 📁 Data 📁 <a id="📁"></a>
+
+# ### 🍃 Extract <a id="🍃"></a>
+
+df = pd.read_csv("data/student-mat.csv")
+
+# ### 💧🔥 Transform <a id="💧🔥"></a>
 
 # +
-df = pd.read_csv("data/student-mat.csv")
 df = df.loc[0:, "school":"guardian"]
 
 cap = lambda s: s.capitalize()
+columns = ["Mjob", "Fjob"]
+df[columns] = df[columns].applymap(cap)
+
+majority = lambda age: age > 17
+df["legal_drinker"] = df["age"].apply(majority)
+
+why = lambda x: 10 * x if type(x) is int else x
+df = df.applymap(why)
 # -
 
-# ## ❄ Step 1. Import the necessary libraries ❄
-#
-# ## 🌸 Solution 🌸
-#
-# See [🐍 Python imports 🐍](#🐍) section.
+# ### 🍵 Load <a id="🍵"></a>
 
-# ## ❄ Step 2. Import the dataset from this [address](https://raw.githubusercontent.com/guipsamora/pandas_exercises/master/04_Apply/Students_Alcohol_Consumption/student-mat.csv). ❄
-#
-# ## 🌸 Solution 🌸
-#
-# See [📁 Data processing 📁](#📁) section.
+ser = df.iloc[-1, 0:]
+pd.DataFrame(ser).T
 
-# ## ❄ Step 3. Assign it to a variable called df. ❄
-#
-# ## 🌸 Solution 🌸
-#
-# See [📁 Data processing 📁](#📁) section.
+# ## 👀 Problems and solutions 👀 <a id="👀"></a>
 
-# ## ❄ Step 4. For the purpose of this exercise slice the dataframe from 'school' until the 'guardian' column ❄
+# ### ❄ Step 1. Import the necessary libraries <a id="❄1"></a>
 #
-# ## 🌸 Solution 🌸
+# #### 🌸 Solution <a id="🌸1"></a>
 #
-# See [📁 Data processing 📁](#📁) section.
+# Line `1` in [🐍 Python imports 🐍](#🐍)
 
-# ## ❄ Step 5. Create a lambda function that will capitalize strings. ❄
+# ### ❄ Step 2. Import the dataset from this [address](https://raw.githubusercontent.com/guipsamora/pandas_exercises/master/04_Apply/Students_Alcohol_Consumption/student-mat.csv). <a id="❄2"></a>
 #
-# ## 🌸 Solution 🌸
+# #### 🌸 Solution 🌸<a id="🌸2"></a>
 #
-# See [📁 Data processing 📁](#📁) section.
+# `data/student-mat.csv`
 
-# ## ❄ Step 6. Capitalize both Mjob and Fjob. ❄
+# ### ❄ Step 3. Assign it to a variable called df. <a id="❄3"></a>
 #
-# ## 🌸 Solution 🌸
+# #### 🌸 Solution <a id="🌸3"></a>
 #
-# See [📁 Data processing 📁](#📁) section.
+# Line `1` in [🍃 Extract](#🍃)
+
+# ### ❄ Step 4. For the purpose of this exercise slice the dataframe from 'school' until the 'guardian' column <a id="❄4"></a>
+#
+# #### 🌸 Solution <a id="🌸4"></a>
+#
+# Line `1` in [💧🔥 Transform](#💧🔥)
+
+# ### ❄ Step 5. Create a lambda function that will capitalize strings. <a id="❄5"></a>
+#
+# #### 🌸 Solution <a id="🌸5"></a>
+#
+# Line `3` in [💧🔥 Transform](#💧🔥)
+
+# ### ❄ Step 6. Capitalize both Mjob and Fjob. <a id="❄6"></a>
+#
+# #### 🌸 Solution <a id="🌸6"></a>
+#
+# Line `4 -> 5` in [💧🔥 Transform](#💧🔥)
+
+# ### ❄ Step 7. Print the last elements of the data set. <a id="❄7"></a>
+#
+# #### 🌸 Solution <a id="🌸7"></a>
+#
+# Line `1` in [🍵 Load](#🍵)
+
+# ### ❄ Step 8. Did you notice the original dataframe is still lowercase? Why is that? Fix it and capitalize Mjob and Fjob. <a id="❄8"></a>
+#
+# #### 🌸 Solution <a id="🌸8"></a>
+#
+# > Did you notice the original dataframe is still lowercase?
+#
+# Nope. No fix necessary
+
+# ### ❄ Step 9. Create a function called majority that returns a boolean value to a new column called legal_drinker (Consider majority as older than 17 years old) <a id="❄9"></a>
+#
+# #### 🌸 Solution <a id="🌸9"></a>
+#
+# Line `7 -> 8` in [💧🔥 Transform](#💧🔥)
+
+# ### ❄ Step 10. Multiply every number of the dataset by 10. <a id="❄10"></a>
+#
+# #### 🌸 Solution <a id="🌸10"></a>
+#
+# Line `10 -> 11` in [💧🔥 Transform](#💧🔥)
